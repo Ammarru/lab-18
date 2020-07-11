c3e817a8ffb2feaafe3e2bfe07bf9c7f313d8a89
dima04112001 <56406646+dima04112001@users.noreply.github.com>
2020-07-11 09:46:57 +0700

    inventory

---
+++ Ship-Escape/Assets/Scripts/SaveLoadManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+public class SaveLoadManager : MonoBehaviour
+{
+    string filePath;
+    private void Start()
+    {
+        filePath = Application.persistentDataPath + "/save.gamesave";
+    }
+
+    public void SaveGame()
+    {
+
+    }
+    public void LoadGame()
+    {
+
+    }
+    [System.Serializable]
+    public class Save
+    {
+
+    }
+       
+}
